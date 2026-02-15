ddba6a058cab1b0140f11a39f251463e4b483009
ghashumov-cpu <ghashumov@gmail.com>
2026-02-15 18:54:21 +0300

    Day 2: variables and types

---
+++ JavaScript/day2.4.js
@@ -0,0 +1,5 @@
+let name = "Ramis";
+let country = "Russia";
+let profession = "Full Stack Developer";
+
+console.log(`Меня зовут ${name}, я живу в ${country} и хочу стать ${profession}`);
---
+++ JavaScript/day2.js
@@ -0,0 +1,33 @@
+//let name = "Ramis";      // строка (text)
+//let age = 20;            // число (number)
+//let isStudent = true;    // логика (boolean)
+
+//console.log(name);
+//console.log(age);
+//console.log(isStudent);
+
+
+//let name = "Ramis";
+//let age = 20;
+
+//console.log(`Меня зовут ${name}, мне ${age} лет`)
+
+let a = 10;
+let b = 5;
+
+console.log (a + b);
+
+let v = 13;
+let g = 16;
+
+console.log (v * g);
+
+let i = 16;
+let p = 4;
+
+console.log (i / p);
+
+let h = 29;
+let n = 18;
+
+console.log (h - n)
